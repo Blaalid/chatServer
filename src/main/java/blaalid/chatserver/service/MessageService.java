b1b935e3a135ecd8d2460c9bdd33dd89727b97b5
Inge Blaalid <Blaalid@me.com>
2016-10-13 21:43:04 +0200

    Deleted message to add a new field

--- src/main/java/blaalid/chatserver/service/MessageService.java
+++ src/main/java/blaalid/chatserver/service/MessageService.java
@@ -1,10 +1,13 @@
 package blaalid.chatserver.service;
 
 
+import blaalid.chatserver.model.Contact;
 import blaalid.chatserver.model.Message;
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
 
 /**
  *
@@ -13,14 +16,15 @@ import java.util.List;
 public class MessageService {
     List<Message> mList = new ArrayList<>();
     
+    @GET
+    @Path("message/create")
     public List<Message> getAllMessages(){
         Message msg1 = new Message(1L, "ja hei du", 1123, "Inge");
         mList.add(msg1);
         return mList;
     }
     
-    @GET
-    @Path("message/create")
+    
     public Message createMessage(
             @QueryParam("userid") Long userid, 
             @QueryParam("receiver") Long recipientid,
@@ -30,21 +34,21 @@ public class MessageService {
         System.out.println("recipientid: " + recipientid );
         System.out.println("text: " + text );
         Contact sender = em.find(Contact.class, userid);
-        Contact recipient = em.find(Contact.class,recipientid);
+        Contact recipient = em.find(Contact.class, recipientid);
         if(sender == null)
         {
-            System.out.println("SENDER ER NULL");
+            System.out.println("No sender");
         }
         if(recipient == null)
         {
-            System.out.println("recipient ER NULL");
+            System.out.println("No recipient");
         }
        
         //TODO: Check for null
         Message message = new Message(sender, recipient, text);
         if((message == null))
         {
-             System.out.print("JEG ER NULLLL!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+             System.out.print("No message");
              
         }
      em.persist(message);
